e576abc667c772804f14ad776af951e8d7be9ab2
Braulio21-bit <Braulio_21@icloud.com>
2025-10-03 18:08:06 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-
-const CACHE="yazy-skin-custommsg-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))) });
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
+self.addEventListener('fetch',()=>{});
